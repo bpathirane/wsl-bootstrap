ae2405a2bb6463e2eebe8e50dfde27c1c964ba3d
Buddhi Pathirane <2478087+bpathirane@users.noreply.github.com>
2026-03-26 11:51:02 -0400

    add krb5-user cifs-utils to bootstrap

--- linux/install-packages.sh
+++ linux/install-packages.sh
@@ -5,8 +5,8 @@ source "$(dirname "$0")/lib.sh"
 apt_update_if_stale
 
 BASE_PACKAGES=(
-  age bat bison build-essential ca-certificates curl direnv fd-find
-  git gnupg htop jq libevent-dev lsb-release ncurses-dev pkg-config ripgrep unzip wslu zsh
+  age bat bison build-essential ca-certificates cifs-utils curl direnv fd-find
+  git gnupg htop jq krb5-user libevent-dev lsb-release ncurses-dev pkg-config ripgrep unzip wslu zsh
 )
 
 for pkg in "${BASE_PACKAGES[@]}"; do
